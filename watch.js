74e57adaceeaaefe7959eef6d4b4e8b7af07711d
belammuia <belammuia0@gmail.com>
2023-04-10 11:44:42 +0300

    updates

--- watch.js
+++ watch.js
@@ -1,16 +1,40 @@
+const searchBtn = document.getElementById('Movies');
+
+searchBtn.addEventListener('click', getName);
+
+function getName() {
+  fetch('http://localhost:3000/films')
+    .then(res => res.json())
+    .then(data => {
+      let html = '';
+      data.forEach(movie => {
+        html += `
+          <h2>${movie.title}</h2>
+        `;
+      });
+      document.getElementById('Movies').innerHTML = html;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+ 
+}
+
+
 function bookTicket(ticket) {
     const cardBox = document.createElement('li');
     cardBox.className = 'box';
     cardBox.innerHTML = `
+ <div class="wrapper">
       <div class="container">
         <img src="${ticket.poster}">
       
         <p>Capacity: ${ticket.capacity}</p>
         <p class="buttonMe">Tickets: ${ticket.tickets_sold}</p>
-        <button class="buy-button">Buy</button>
-      <button class="remove">Return</button>
+        <button class="buy-button">Book</button>
+        <button class="buy"
       </div>
-      
+      </div>
     `;
     return cardBox;
   }
@@ -32,8 +56,8 @@ function bookTicket(ticket) {
             const ticketsSold = parseInt(buttonMe.textContent.split(':')[1]);
             const newTicketsSold = Math.abs(ticketsSold - 1);
             buttonMe.textContent = `Tickets: ${newTicketsSold}`;
-            if (newTicketsSold === 0) {
-              alert('Sorry, no more tickets are available.');
+            if (newTicketsSold === ticketsSold-1) {
+              alert('Note you can only buy only one ticket');
               button.disabled = true;
             }
           });
@@ -45,4 +69,5 @@ function bookTicket(ticket) {
   }
   
   buyTicket();
+  //
   
